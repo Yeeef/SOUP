05245894e8980b16830b8399220f434001faea81
ahaWDY <2503131624@qq.com>
2019-06-04 20:11:37 +0800

    finish lex and syntax report

---
+++ yapc/test/test_errorh.pas
@@ -0,0 +1,15 @@
+program test_1;
+type
+    arr = array [050] of integer;
+  	MailingListRecord = record
+    	FirstName: string;
+    	LastName: string;
+    	Address: string;
+    	City: string;
+    	State: string;
+    	Zip: Integer;
+  	end;
+
+begin
+
+end.
